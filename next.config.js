8d6436bb305d983fb5a92fc2d2fca559d79ef521
Georg But <georg.but@revampit.ch>
2025-04-29 11:18:19 +0200

    fix: update configuration for proper CSS and font handling

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'], // For Sanity.io image hosting
   },
+  experimental: {
+    optimizeCss: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'static/fonts/',
+        },
+      },
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
